4aa4fa5ea9d2a4acefc573f15fc6dd72784b2b06
James Hiew <james@hiew.net>
2023-12-23 10:20:59 +0000

    Put world generators into individual files

---
+++ src/lib/extras/worldgen/flat.rs
@@ -0,0 +1,25 @@
+use crate::common::chunks::Chunk;
+use crate::common::world::{BlockId, ChunkBlockId, ChunkPosition, WorldGen};
+use crate::extras::block_ids::DIRT_BLOCK_ID;
+use crate::extras::chunks;
+use std::collections::HashMap;
+
+#[derive(Debug, Default, Clone, PartialEq, Eq)]
+pub struct Flat {
+    pub block_mappings: HashMap<BlockId, ChunkBlockId>,
+}
+
+impl WorldGen for Flat {
+    fn initialize(&mut self, mappings: HashMap<BlockId, ChunkBlockId>) {
+        self.block_mappings = mappings;
+    }
+
+    fn get(&self, pos: &ChunkPosition, _zoom: f64) -> Chunk {
+        // zoom doesn't change anything
+        if pos.y == -1 {
+            chunks::top_chunk(*self.block_mappings.get(DIRT_BLOCK_ID).unwrap()).into()
+        } else {
+            Chunk::Empty
+        }
+    }
+}
---
+++ src/lib/extras/worldgen/single_block.rs
@@ -0,0 +1,29 @@
+use crate::common::chunks::{Chunk, UnpackedChunk, CHUNK_SIZE};
+use crate::common::world::{BlockId, BlockPosition, ChunkBlockId, ChunkPosition, WorldGen};
+use crate::extras::block_ids::GRASS_BLOCK_ID;
+use std::collections::HashMap;
+
+#[derive(Debug, Default, Clone, PartialEq, Eq)]
+pub struct SingleBlock {
+    pub block_mappings: HashMap<BlockId, ChunkBlockId>,
+}
+
+impl WorldGen for SingleBlock {
+    fn initialize(&mut self, mappings: HashMap<BlockId, ChunkBlockId>) {
+        self.block_mappings = mappings;
+    }
+
+    fn get(&self, _pos: &ChunkPosition, _zoom: f64) -> Chunk {
+        // TODO: implement zoom?
+        let mut chunk = UnpackedChunk::default();
+        chunk.insert(
+            &BlockPosition {
+                x: CHUNK_SIZE / 2,
+                y: 0,
+                z: CHUNK_SIZE / 2,
+            },
+            *self.block_mappings.get(GRASS_BLOCK_ID).unwrap(),
+        );
+        chunk.into()
+    }
+}
