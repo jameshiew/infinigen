0010e942829e3888158f460e59ea59a517e661fe
James Hiew <james@hiew.net>
2024-12-01 00:34:28 +0000

    Switch benchmark to mountain islands

--- crates/common/benches/benchmark.rs
+++ crates/common/benches/benchmark.rs
@@ -1,15 +1,15 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use infinigen_common::extras::worldgen::mountain_islands::MountainIslands;
 use infinigen_common::world::{ChunkPosition, WorldGen};
 use infinigen_common::zoom::ZoomLevel;
 
 use infinigen_common::extras::block_ids::default_block_ids;
-use infinigen_common::extras::worldgen::layered::Layered;
 
-fn bench_layered(c: &mut Criterion) {
-    let mut wgen = Layered::default();
+fn bench_mountain_islands(c: &mut Criterion) {
+    let mut wgen = MountainIslands::default();
     wgen.initialize(default_block_ids());
     const SIZE: i32 = 3;
-    c.bench_function("layered", |b| {
+    c.bench_function("mountain islands", |b| {
         b.iter(|| {
             for cx in -SIZE..SIZE {
                 for cy in -SIZE..SIZE {
@@ -29,5 +29,5 @@ fn bench_layered(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, bench_layered,);
+criterion_group!(benches, bench_mountain_islands,);
 criterion_main!(benches);
