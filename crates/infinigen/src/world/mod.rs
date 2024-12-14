81b8c68895345a359b66bc48a6152b908dd9f8b5
James Hiew <james@hiew.net>
2024-12-14 19:38:53 +0000

    Add AppState::InitializingWorld

--- crates/infinigen/src/world/mod.rs
+++ crates/infinigen/src/world/mod.rs
@@ -6,6 +6,10 @@ use infinigen_common::world::{ChunkPosition, WorldGen};
 use infinigen_common::zoom::ZoomLevel;
 use infinigen_extras::worldgen;
 
+use crate::assets::blocks::BlockRegistry;
+use crate::settings::Config;
+use crate::AppState;
+
 #[derive(Resource)]
 pub struct World {
     pub generator: Arc<Box<dyn WorldGen + Send + Sync>>,
@@ -31,6 +35,20 @@ pub struct WorldPlugin;
 impl Plugin for WorldPlugin {
     fn build(&self, app: &mut App) {
         tracing::info!("Initializing world plugin");
-        app.init_resource::<World>();
+        app.init_resource::<World>()
+            .add_systems(OnEnter(AppState::InitializingWorld), init_world);
     }
+}
+
+fn init_world(
+    mut next_state: ResMut<NextState<AppState>>,
+    registry: Res<BlockRegistry>,
+    config: Res<Config>,
+    mut world: ResMut<World>,
+) {
+    let mut worldgen: Box<dyn infinigen_common::world::WorldGen + Send + Sync> =
+        config.world.into();
+    worldgen.initialize((&registry.block_mappings).into());
+    world.generator = Arc::new(worldgen);
+    next_state.set(AppState::MainGame);
 }
