dbf03cec2f388a8dd0b1deeb4727cacfe397fdb5
James Hiew <james@hiew.net>
2024-12-16 20:44:34 +0000

    jemalloc

--- crates/infinigen/src/global_allocator.rs
+++ crates/infinigen/src/global_allocator.rs
@@ -1,21 +1,4 @@
-#[cfg(all(
-    feature = "jemalloc",
-    not(target_env = "msvc"),
-    not(target_family = "wasm")
-))]
 use tikv_jemallocator::Jemalloc;
 
-#[cfg(all(
-    feature = "jemalloc",
-    not(target_env = "msvc"),
-    not(target_family = "wasm")
-))]
 #[global_allocator]
 static GLOBAL: Jemalloc = Jemalloc;
-
-pub fn check_global_allocator() {
-    #[cfg(all(feature = "jemalloc", target_env = "msvc"))]
-    panic!("Jemalloc feature does not work on Windows");
-    #[cfg(all(feature = "jemalloc", target_family = "wasm"))]
-    panic!("Jemalloc feature does not work on WASM");
-}
