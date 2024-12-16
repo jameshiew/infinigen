f49153bbd2891bb4a8f7ddc5fe56cfc154080e5a
James Hiew <james@hiew.net>
2024-12-16 18:04:28 +0000

    Split global allocator into separate module

--- crates/infinigen/src/main.rs
+++ crates/infinigen/src/main.rs
@@ -9,27 +9,16 @@ use bevy::tasks::available_parallelism;
 use bevy::window::{Window, WindowPlugin};
 use bevy::DefaultPlugins;
 use config::Config;
+use infinigen::global_allocator::check_global_allocator;
 use infinigen::AppPlugin;
-#[cfg(all(
-    feature = "jemalloc",
-    not(target_env = "msvc"),
-    not(target_family = "wasm")
-))]
-use tikv_jemallocator::Jemalloc;
-
-#[cfg(all(
-    feature = "jemalloc",
-    not(target_env = "msvc"),
-    not(target_family = "wasm")
-))]
-#[global_allocator]
-static GLOBAL: Jemalloc = Jemalloc;
 
 const APP_NAME: &str = "infinigen";
 const CONFIG_PREFIX: &str = "infinigen_";
 const DEFAULT_LOG_FILTER: &str = "info,wgpu_core=warn,wgpu_hal=warn,naga=info";
 
 fn main() -> ExitCode {
+    check_global_allocator();
+
     let cfg = Config::builder()
         .add_source(config::File::with_name("config"))
         .add_source(config::Environment::with_prefix(CONFIG_PREFIX))
