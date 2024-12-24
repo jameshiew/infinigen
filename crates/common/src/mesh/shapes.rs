fa0ab8b1d01c464caf0e64edfa76c103209fede5
James Hiew <james@hiew.net>
2024-12-24 15:24:13 +0000

    Stub benchmarks for meshing functions

--- crates/common/src/mesh/shapes.rs
+++ crates/common/src/mesh/shapes.rs
@@ -10,13 +10,13 @@ pub const PADDED_CHUNK_SIZE: u32 = CHUNK_SIZE_U32 + 2;
 pub const PADDED_CHUNK_MAX_INDEX: u32 = PADDED_CHUNK_SIZE - 1;
 
 pub type PaddedChunkShape = ConstShape3u32<PADDED_CHUNK_SIZE, PADDED_CHUNK_SIZE, PADDED_CHUNK_SIZE>;
-pub type PaddedChunk = [VoxelBlock; PaddedChunkShape::SIZE as usize];
+pub type PaddedChunk = [VoxelBlock; PaddedChunkShape::USIZE];
 
 /// Represents a face of a chunk - when meshing a chunk, we need the faces of the neighboring chunks.
 pub type ChunkFaceShape = ConstShape2u32<CHUNK_SIZE_U32, CHUNK_SIZE_U32>;
-pub type ChunkFace = [VoxelBlock; ChunkFaceShape::SIZE as usize];
+pub type ChunkFace = [VoxelBlock; ChunkFaceShape::USIZE];
 
-pub const EMPTY_CHUNK_FACE: ChunkFace = [VoxelBlock::Empty; ChunkFaceShape::SIZE as usize];
+pub const EMPTY_CHUNK_FACE: ChunkFace = [VoxelBlock::Empty; ChunkFaceShape::USIZE];
 pub const EMPTY_CHUNK_FACES: StaticCopyMap<Direction, ChunkFace> = static_copy_map! {
     of type Direction:
     _ => EMPTY_CHUNK_FACE,
