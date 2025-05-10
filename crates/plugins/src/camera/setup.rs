aade9589183a256875774f9e9496e48e1b1270e6
James Hiew <james@hiew.net>
2025-05-10 11:39:18 +0100

    Switch to smooth-bevy-camera

--- crates/plugins/src/camera/setup.rs
+++ crates/plugins/src/camera/setup.rs
@@ -3,33 +3,41 @@ use bevy::core_pipeline::experimental::taa::TemporalAntiAliasing;
 use bevy::pbr::ScreenSpaceAmbientOcclusion;
 use bevy::prelude::*;
 use bevy::render::view::GpuCulling;
-use bevy_flycam::FlyCam;
+use smooth_bevy_cameras::controllers::fps::{FpsCameraBundle, FpsCameraController};
 
 #[derive(Resource)]
 pub struct CameraSettings {
     pub zoom_level: i8,
-    pub rotation_x: f32,
-    pub rotation_y: f32,
-    pub rotation_z: f32,
-    pub rotation_w: f32,
     pub wx: f32,
     pub wy: f32,
     pub wz: f32,
+    pub target_x: f32,
+    pub target_y: f32,
+    pub target_z: f32,
 }
 
 pub fn setup(mut commands: Commands, settings: Res<CameraSettings>) {
     let zoom = 2f32.powf(settings.zoom_level as f32);
-    let mut transform =
-        Transform::from_xyz(settings.wx * zoom, settings.wy * zoom, settings.wz * zoom);
-    transform.rotation.x = settings.rotation_x;
-    transform.rotation.y = settings.rotation_y;
-    transform.rotation.z = settings.rotation_z;
-    transform.rotation.w = settings.rotation_w;
-    transform.rotation = transform.rotation.normalize();
+
+    let eye = Transform::from_xyz(settings.wx * zoom, settings.wy * zoom, settings.wz * zoom);
+    let target = Transform::from_xyz(
+        settings.target_x * zoom,
+        settings.target_y * zoom,
+        settings.target_z * zoom,
+    );
     commands.spawn((
         Name::new("Camera"),
-        FlyCam,
-        transform,
+        FpsCameraBundle::new(
+            FpsCameraController {
+                enabled: true,
+                mouse_rotate_sensitivity: Vec2::splat(0.2),
+                translate_sensitivity: 30.0,
+                smoothing_weight: 0.7,
+            },
+            eye.translation,
+            target.translation,
+            Vec3::Y,
+        ),
         Camera3d::default(),
         ContrastAdaptiveSharpening::default(),
         ScreenSpaceAmbientOcclusion::default(),
