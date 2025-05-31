b95915a21e78d9676e81b927e703d7bc05598e94
James Hiew <james@hiew.net>
2025-05-31 10:47:21 +0100

    Setup cursor handling at startup; rename funcs

--- crates/plugins/src/controls/mod.rs
+++ crates/plugins/src/controls/mod.rs
@@ -1,17 +1,14 @@
-//! Handles grabbing the cursor for the window.
-
 use bevy::prelude::*;
 use bevy::window::{CursorGrabMode, PrimaryWindow, Window};
 
-use crate::AppState;
 use crate::camera::events::CameraEvent;
 
-pub fn setup(mut primary_window: Single<&mut Window, With<PrimaryWindow>>) {
+pub fn setup_cursor(mut primary_window: Single<&mut Window, With<PrimaryWindow>>) {
     primary_window.cursor_options.grab_mode = CursorGrabMode::Confined;
     primary_window.cursor_options.visible = false;
 }
 
-pub fn handle_input(
+pub fn handle_cursor_focus(
     keyboard: Res<ButtonInput<KeyCode>>,
     mut primary_window: Single<&mut Window, With<PrimaryWindow>>,
     mut camera_events: EventWriter<CameraEvent>,
@@ -36,7 +33,7 @@ pub struct ControlsPlugin;
 
 impl Plugin for ControlsPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(OnEnter(AppState::MainGame), setup)
-            .add_systems(Update, handle_input);
+        app.add_systems(Startup, setup_cursor)
+            .add_systems(Update, handle_cursor_focus);
     }
 }
