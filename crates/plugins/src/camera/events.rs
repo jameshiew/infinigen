9511225df9cfeea7cb40e47e372e52c406e43a9e
James Hiew <james@hiew.net>
2025-06-01 10:44:21 +0100

    Bound handling camera events to main game state

--- crates/plugins/src/camera/events.rs
+++ crates/plugins/src/camera/events.rs
@@ -9,11 +9,8 @@ pub enum CameraEvent {
 
 pub fn handle_camera_events(
     mut camera_events: EventReader<CameraEvent>,
-    fps_camera_controls: Option<Single<&mut FpsCameraController>>,
+    mut fps_camera_controls: Single<&mut FpsCameraController>,
 ) {
-    let Some(mut fps_camera_controls) = fps_camera_controls else {
-        return;
-    };
     for ev in camera_events.read() {
         match ev {
             CameraEvent::EnableControls => fps_camera_controls.enabled = true,
