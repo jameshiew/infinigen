eb3b5999fa4feb205988f80b47853d7d68cefaec
James Hiew <james@hiew.net>
2025-10-16 11:54:32 +0100

    Clean up changing cursor .grab_mode and .visible

--- crates/plugins/src/camera/events.rs
+++ crates/plugins/src/camera/events.rs
@@ -1,5 +1,4 @@
 use bevy::prelude::*;
-use bevy::window::CursorGrabMode;
 
 use super::FpsController;
 
@@ -12,22 +11,11 @@ pub enum CameraEvent {
 pub fn handle_camera_events(
     mut camera_events: EventReader<CameraEvent>,
     mut fps_controller: Single<&mut FpsController>,
-    mut windows: Single<&mut Window>,
 ) {
     for ev in camera_events.read() {
         match ev {
-            CameraEvent::EnableControls => {
-                fps_controller.enabled = true;
-                // Grab cursor when enabling controls
-                windows.cursor_options.grab_mode = CursorGrabMode::Locked;
-                windows.cursor_options.visible = false;
-            }
-            CameraEvent::DisableControls => {
-                fps_controller.enabled = false;
-                // Release cursor when disabling controls
-                windows.cursor_options.grab_mode = CursorGrabMode::None;
-                windows.cursor_options.visible = true;
-            }
+            CameraEvent::EnableControls => fps_controller.enabled = true,
+            CameraEvent::DisableControls => fps_controller.enabled = false,
         }
     }
 }
